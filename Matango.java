952ef616a1b35a792509b4b7c2e763f07e558a39
mjpurin <mjpurin@gmail.com>
2023-06-07 13:12:00 +0900

    10章まで

--- Matango.java
+++ Matango.java
@@ -1,7 +1,17 @@
 public class Matango{
-	int hp;
+	int hp=50;
 	final int LEVEL=10;
 	char suffix;
+
+	public Matango(char suffix){
+		this.suffix=suffix;
+	}
+
+	public void attack(Hero h){
+		System.out.println("おばけキノコ"+this.suffix+"の攻撃");
+		System.out.println("10のダメージ");
+		h.hp -= 10;
+	}
 
 	public void run(){
 		System.out.println("おばけキノコ"+this.suffix+"は逃げ出した");
@@ -9,5 +19,22 @@ public class Matango{
 	public void appear(){
 		System.out.printf("おばけキノコ%s(%d)があらわれた!%n",this.suffix,this.hp);
 
+	}
+}
+class PoisonMatango extends Matango{
+	int poisonCount = 5;
+	public PoisonMatango(char suffix){
+		super(suffix);
+	}
+	@Override
+	public void attack(Hero h){
+		super.attack(h);
+		if(this.poisonCount > 0){
+			System.out.println("さらに毒の胞子をばらまいた!");
+			int damage = h.hp/5;
+			h.hp -= damage;
+			System.out.println(damage+"ポイントのダメージ!");
+			this.poisonCount--;
+		}
 	}
 }
